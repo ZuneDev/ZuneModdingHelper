7ca0440095967cdb790e3a0b531217ba38fdea84
Joshua Askharoun <jjask7@gmail.com>
2022-03-01 15:09:26 -0600

    Added UI for dependencies

--- ZuneModCore/ModDependency.cs
+++ ZuneModCore/ModDependency.cs
@@ -7,15 +7,17 @@ namespace ZuneModCore
 {
     public class ModDependency
     {
-        public string Id { get; set; }
-
-        public ReleaseVersion Version { get; set; }
-
         public ModDependency(string id, ReleaseVersion? version = null)
         {
             Id = id;
             Version = version ?? ModManager.CurrentVersion;
         }
+
+        public string Id { get; set; }
+
+        public ReleaseVersion Version { get; set; }
+
+        public bool Status => CheckStatus();
 
         /// <summary>
         /// DO NOT USE DIRECTLY. This constructor is only for
@@ -24,7 +26,9 @@ namespace ZuneModCore
         [Obsolete]
         internal ModDependency() { }
 
-        public Task<bool> CheckStatus() => ModManager.CheckStatus(Id);
+        public Task<bool> CheckStatusAsync() => ModManager.CheckStatus(Id);
+
+        private bool CheckStatus() => CheckStatusAsync().Result;
 
         public override string ToString() => $"{Id} / {Version}";
     }
---
+++ ZuneModdingHelper/Converters/BoolToSymbolConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ZuneModdingHelper.Converters
+{
+    internal class BoolToSymbolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not bool val)
+                return null;
+
+            var resultsStr = parameter.ToString().Split(',');
+            object trueReturnVal = null;
+            object falseReturnVal = null;
+
+            if (targetType == typeof(string))
+            {
+                trueReturnVal = resultsStr[0];
+                falseReturnVal = resultsStr[1];
+            }
+            else if (targetType == typeof(Brush))
+            {
+                var brushConverter = new BrushConverter();
+                trueReturnVal = brushConverter.ConvertFrom(resultsStr[0]);
+                falseReturnVal = brushConverter.ConvertFrom(resultsStr[1]);
+            }
+
+            return val ? trueReturnVal : falseReturnVal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var results = parameter.ToString().Split(',');
+            return value.ToString() == results[0];
+        }
+    }
+}
