5db9421bfd9984a87767f02b8277e8384926ea83
Yoshi Askharoun <jjask7@gmail.com>
2024-05-12 01:40:12 -0500

    Add TextButton

---
+++ ZuneModdingHelper/Controls/TextButton.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace ZuneModdingHelper.Controls;
+
+public class TextButton : ToggleButton
+{
+    static TextButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(TextButton), new FrameworkPropertyMetadata(typeof(TextButton)));
+    }
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+    public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+        nameof(Text), typeof(string), typeof(TextButton), new PropertyMetadata(string.Empty));
+
+    public bool AllowUncheck
+    {
+        get => (bool)GetValue(AllowUncheckProperty);
+        set => SetValue(AllowUncheckProperty, value);
+    }
+    public static readonly DependencyProperty AllowUncheckProperty = DependencyProperty.Register(
+        nameof(AllowUncheck), typeof(bool), typeof(TextButton), new PropertyMetadata(true));
+
+    protected override void OnChecked(RoutedEventArgs e)
+    {
+        if (!AllowUncheck)
+            IsEnabled = false;
+
+        base.OnChecked(e);
+    }
+
+    protected override void OnUnchecked(RoutedEventArgs e)
+    {
+        IsEnabled = true;
+        base.OnUnchecked(e);
+    }
+}
