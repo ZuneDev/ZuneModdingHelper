a1359eb4fa8521ae37634cdb4c5e7bcb24664b36
Yoshi Askharoun <jjask7@gmail.com>
2024-05-12 00:01:42 -0500

    Implement caption buttons

--- ZuneModdingHelper/AppWindow.xaml.cs
+++ ZuneModdingHelper/AppWindow.xaml.cs
@@ -1,9 +1,6 @@
-﻿using ControlzEx.Behaviors;
-using ControlzEx.Controls.Internal;
-using System;
+﻿using System;
 using System.Windows;
 using System.Windows.Interop;
-using System.Windows.Shell;
 
 namespace ZuneModdingHelper
 {
@@ -23,6 +20,26 @@ namespace ZuneModdingHelper
         {
             base.OnSourceInitialized(e);
             _windowHandle = new WindowInteropHelper(this).EnsureHandle();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e) => SetWindowState(WindowState.Minimized);
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetWindowState(WindowState == WindowState.Normal
+                ? WindowState.Maximized : WindowState.Normal);
+
+            BorderThickness = new Thickness(WindowState == WindowState.Normal ? 0 : 8);
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void SetWindowState(WindowState state)
+        {
+            // Hack for preserving animations and window sizing (don't cover the taskbar!)
+            WindowStyle = WindowStyle.SingleBorderWindow;
+            WindowState = state;
+            WindowStyle = WindowStyle.None;
         }
     }
 }
