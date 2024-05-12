6a350433c540540eb0f313c0a3518ba8af65ab70
Yoshi Askharoun <jjask7@gmail.com>
2024-05-12 11:35:44 -0500

    Implement pivot selection

--- ZuneModdingHelper/AppWindow.xaml.cs
+++ ZuneModdingHelper/AppWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Interop;
 
 namespace ZuneModdingHelper
@@ -10,10 +12,19 @@ namespace ZuneModdingHelper
     public partial class AppWindow
     {
         private IntPtr _windowHandle;
+        private int _selectedPivotIdx = 0;
 
         public AppWindow()
         {
+            Loaded += AppWindow_Loaded;
             InitializeComponent();
+        }
+
+        private void AppWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Select MODS tab
+            var pivotItem = (ToggleButton)Pivot.Children[_selectedPivotIdx];
+            pivotItem.IsChecked = true;
         }
 
         protected override void OnSourceInitialized(EventArgs e)
@@ -40,6 +51,20 @@ namespace ZuneModdingHelper
             WindowStyle = WindowStyle.SingleBorderWindow;
             WindowState = state;
             WindowStyle = WindowStyle.None;
+        }
+
+        private void PivotButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var newIdx = Grid.GetColumn((UIElement)sender);
+            if (newIdx == _selectedPivotIdx)
+                return;
+
+            var pivotItem = (ToggleButton)Pivot.Children[_selectedPivotIdx];
+            pivotItem.IsChecked = false;
+
+            _selectedPivotIdx = newIdx;
+            pivotItem = (ToggleButton)Pivot.Children[_selectedPivotIdx];
+            pivotItem.IsChecked = true;
         }
     }
 }
