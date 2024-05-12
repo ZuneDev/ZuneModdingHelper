339665db9175607c7f0eeeb8ba15c0ac1f59a7f1
Yoshi Askharoun <jjask7@gmail.com>
2024-05-11 23:37:01 -0500

    Begin Zune-themed window

---
+++ ZuneModdingHelper/AppWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ControlzEx.Behaviors;
+using ControlzEx.Controls.Internal;
+using System;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Shell;
+
+namespace ZuneModdingHelper
+{
+    /// <summary>
+    /// Interaction logic for AppWindow.xaml
+    /// </summary>
+    public partial class AppWindow
+    {
+        private IntPtr _windowHandle;
+
+        public AppWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            _windowHandle = new WindowInteropHelper(this).EnsureHandle();
+        }
+    }
+}
