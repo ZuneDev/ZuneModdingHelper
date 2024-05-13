58bbc61619efafde6bc1a613235229bd4c02a8dc
Yoshi Askharoun <jjask7@gmail.com>
2024-05-13 12:19:21 -0500

    Fix mod list style, remove management buttons

--- ZuneModdingHelper/Pages/ModsPage.xaml.cs
+++ ZuneModdingHelper/Pages/ModsPage.xaml.cs
@@ -1,19 +1,4 @@
-﻿using Microsoft.AppCenter.Analytics;
-using OwlCore.AbstractUI.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 using ZuneModCore;
 
 namespace ZuneModdingHelper.Pages
@@ -27,20 +12,6 @@ namespace ZuneModdingHelper.Pages
         {
             InitializeComponent();
             ModList.ItemsSource = Mod.AvailableMods;
-        }
-
-        private async void ModInstallButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is FrameworkElement elem && elem.DataContext is Mod mod)
-            {
-            }
-        }
-
-        private async void ModResetButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is FrameworkElement elem && elem.DataContext is Mod mod)
-            {
-            }
         }
     }
 }
