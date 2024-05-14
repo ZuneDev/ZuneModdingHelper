d0c8c99e05f9002cc731af671743068330a9c7d4
Yoshi Askharoun <jjask7@gmail.com>
2024-05-13 22:51:26 -0500

    Implement About page

--- ZuneModdingHelper/Pages/AboutPage.xaml.cs
+++ ZuneModdingHelper/Pages/AboutPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+﻿using System.Windows.Controls;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ZuneModdingHelper.Pages
 {
@@ -23,6 +11,12 @@ namespace ZuneModdingHelper.Pages
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        private void Link_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            App.OpenInBrowser(e.Uri.AbsoluteUri);
+            e.Handled = true;
         }
     }
 }
