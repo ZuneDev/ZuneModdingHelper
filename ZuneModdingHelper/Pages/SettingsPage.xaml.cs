e431dd9a9e44527c784227bc35e6917f0565dddc
Yoshi Askharoun <jjask7@gmail.com>
2024-05-17 21:55:07 -0500

    Add folder picker button

--- ZuneModdingHelper/Pages/SettingsPage.xaml.cs
+++ ZuneModdingHelper/Pages/SettingsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Windows;
 using System.Windows.Controls;
 using ZuneModdingHelper.Services;
 
@@ -9,15 +10,45 @@ namespace ZuneModdingHelper.Pages
     /// </summary>
     public partial class SettingsPage : UserControl
     {
-        public SettingsPage()
+        public SettingsPage(Settings settings)
         {
+            ViewModel = new()
+            {
+                Settings = settings,
+                FolderPickerVisibility = CommonFileDialog.IsPlatformSupported
+                    ? Visibility.Visible : Visibility.Collapsed,
+            };
+            DataContext = ViewModel;
+
             InitializeComponent();
-            DataContext = Settings.Default;
         }
+
+        private SettingsViewModel ViewModel { get; }
 
         private async void SettingsPage_Unloaded(object sender, RoutedEventArgs e)
         {
-            await Settings.Default.SaveAsync();
+            await ViewModel.Settings.SaveAsync();
         }
+
+        private void LocateZuneButton_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new()
+            {
+                IsFolderPicker = true,
+                DefaultDirectory = ViewModel.Settings.ZuneInstallDir
+            };
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+            {
+                ViewModel.Settings.ZuneInstallDir = dialog.FileName;
+            }
+        }
+    }
+
+    internal class SettingsViewModel
+    {
+        public Settings Settings { get; init; }
+
+        public Visibility FolderPickerVisibility { get; init; }
     }
 }
