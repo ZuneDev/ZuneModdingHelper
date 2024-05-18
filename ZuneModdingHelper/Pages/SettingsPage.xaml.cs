6a9239eea99c1f51ea17b71a79dcda669e7838d7
Yoshi Askharoun <jjask7@gmail.com>
2024-05-17 22:59:12 -0500

    Add KILL ALL button

--- ZuneModdingHelper/Pages/SettingsPage.xaml.cs
+++ ZuneModdingHelper/Pages/SettingsPage.xaml.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.WindowsAPICodePack.Dialogs;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using ZuneModdingHelper.Messages;
 using ZuneModdingHelper.Services;
 
 namespace ZuneModdingHelper.Pages
@@ -42,6 +47,44 @@ namespace ZuneModdingHelper.Pages
             {
                 ViewModel.Settings.ZuneInstallDir = dialog.FileName;
             }
+        }
+
+        private void KillZuneButton_Click(object sender, RoutedEventArgs e)
+        {
+            var curProc = Process.GetCurrentProcess();
+            var zuneProcs = Process.GetProcesses()
+                .Where(p =>
+                    p.ProcessName.Contains("zune", System.StringComparison.OrdinalIgnoreCase)
+                    && p.Id != curProc.Id
+                );
+
+            StringBuilder sb = new("The following processes were killed:\r\n");
+            bool anyKilled = false;
+
+            foreach (var proc in zuneProcs)
+            {
+                var id = proc.Id;
+                var name = proc.ProcessName;
+                var path = OwlCore.Flow.Catch(() => proc.MainModule.FileName);
+
+                try
+                {
+                    proc.Kill(true);
+                    
+                    sb.AppendFormat("    • ({0}) {1}", id, name);
+                    if (path is not null)
+                        sb.AppendFormat(" @ {0}", path);
+                    sb.AppendLine();
+
+                    anyKilled = true;
+                }
+                catch { }
+            }
+
+            WeakReferenceMessenger.Default.Send(new ShowDialogMessage(new()
+            {
+                Description = anyKilled ? sb.ToString() : "No processes were killed.",
+            }));
         }
     }
 
