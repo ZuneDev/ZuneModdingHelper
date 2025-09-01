41576532f6f1fc4162e1f58d86f886c0c95b5e83
Yoshi Askharoun <jjask7@gmail.com>
2025-08-31 18:13:56 -0700

    [WIP] Donation request interval settings

---
+++ OwlCore.Wpf/EnumValuesExtension.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace OwlCore.Wpf;
+
+public class EnumValuesExtension : MarkupExtension
+{
+    public Type Enum { get; set; }
+
+    public override object ProvideValue(IServiceProvider serviceProvider) => System.Enum.GetValues(Enum);
+}
---
+++ ZuneModdingHelper/Controls/DonationIntervalDescriptionConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using ZuneModdingHelper.Services;
+
+namespace ZuneModdingHelper.Controls;
+
+public class DonationIntervalDescriptionConverter : DependencyObject, IValueConverter
+{
+    /// <inheritdoc/>
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            DonationRequestInterval.EveryMod => "After every successful mod",
+            DonationRequestInterval.OneWeek => "Once a week",
+            DonationRequestInterval.TwoWeeks => "Once every two weeks",
+            DonationRequestInterval.OneMonth => "Once a month",
+            DonationRequestInterval.ThreeMonths => "Once a quarter",
+            DonationRequestInterval.SixMonths => "Once every six months",
+            DonationRequestInterval.OneYear => "Once a year",
+            _ => value?.ToString(),
+        };
+    }
+
+    /// <inheritdoc/>
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
